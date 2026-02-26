862fe24655e7bf672f9162d5835da023d18a26ed
“霁月” <1423154431@qq.com>
2026-02-26 10:40:39 +0800

    安装ts相关依赖，配置ts文件

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,13 @@
   <RouterView />
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { RouterView } from 'vue-router';
+
+export default defineComponent({
   name: 'QuizzoApp',
-};
+});
 </script>
 
 <style src="@/assets/styles/App.scss" lang="scss" />
