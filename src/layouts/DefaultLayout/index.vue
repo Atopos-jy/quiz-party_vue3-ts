86faaf2ea59f333672a3d30d9c3ddfdf4f21d909
“霁月” <1423154431@qq.com>
2026-03-07 15:02:25 +0800

    delete NameInput.scss

--- src/layouts/DefaultLayout/index.vue
+++ src/layouts/DefaultLayout/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <main class="default-layout max-w-[1500px] mx-auto">
+  <!-- default-layout: 最大宽度1500px居中，响应式内边距 -->
+  <main class="max-w-[1500px] mx-auto px-8 py-16 max-xs:px-8 max-xs:py-8">
     <slot />
   </main>
 </template>
